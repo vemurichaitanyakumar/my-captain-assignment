4b7d6e99dffe11f90003362d38ee4ec6cb0123f2
vemurichaitanyakumar <70809329+vemurichaitanyakumar@users.noreply.github.com>
2020-10-01 10:46:35 +0530

    Add files via upload

---
+++ in.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b;
+    int product;
+    cout<<"enter two numbers";
+    cin>>a;
+    cin>>b;
+    product=a*b;
+    cout<<"prodct"<<"="<<product;
+    return 0;
+
+}
+
